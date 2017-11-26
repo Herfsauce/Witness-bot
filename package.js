ff3bfa8577153e412db2e3c9f077341a9a5fd66b
Herfsauce <33992188+Herfsauce@users.noreply.github.com>
2017-11-25 19:51:02 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "Witbot",
+  "description": "WITNESS IS A FAG",
+  "Version": "0.6.9",
+  "main": "witbot.java",
+  "scripts": {
+    "start": "node witbot.js"
+},
+"dependencies": {
+  "discord.js": "11.1.0",
+  }
+}
